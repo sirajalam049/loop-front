ba564413496879f5f0e97d2ac11a9888453587e0
Siraj Alam <sirajalam049@gmail.com>
2019-06-13 14:12:25 +0530

    Base Url Error handling is done

--- src/LoopFront.ts
+++ src/LoopFront.ts
@@ -1,4 +1,3 @@
-import _ from 'lodash'
 import { Dispatch } from 'redux';
 import utils from './utils';
 
@@ -106,7 +105,7 @@ class LoopFront<TCustomActions extends TStringObject = {}, TEntities extends TSt
         this.ModelName = modelName;
 
         // name in the CAPS
-        this.ModelCaps = _.toUpper(modelName)
+        this.ModelCaps = (modelName || '').toUpperCase();
 
         // All the actions including custom actions.
         this.Actions = {
@@ -238,67 +237,67 @@ class LoopFront<TCustomActions extends TStringObject = {}, TEntities extends TSt
 
     requestGetEntityByItem = async (id: number | string, entity: TEntities[keyof TEntities], params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, params });
     getEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.FETCHING_ENTITY_OF_ITEM = `FETCHING_${_.toUpper(entity as string)}_OF_SINGLE_${this.ModelCaps}`;
+        this.Actions.FETCHING_ENTITY_OF_ITEM = `FETCHING_${(entity || '' as string).toUpperCase()}_OF_SINGLE_${this.ModelCaps}`;
         dispatch({ type: this.Actions.FETCHING_ENTITY_OF_ITEM, entity });
         const response = await this.requestGetEntityByItem(id, entity, params).catch(utils.throwError);
-        this.Actions.ENTITY_OF_ITEM_RECEIVED = `${_.toUpper(entity)}_OF_SINGLE_${this.ModelCaps}_RECEIVED`;
+        this.Actions.ENTITY_OF_ITEM_RECEIVED = `${(entity || '').toUpperCase()}_OF_SINGLE_${this.ModelCaps}_RECEIVED`;
         dispatch({ type: this.Actions.ENTITY_OF_ITEM_RECEIVED, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
     requestPostEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, ) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, data, method: 'POST', params });
     postEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.POSTING_ENTITY_OF_ITEM = `POSTING_${_.toUpper(entity)}_OF_${this.ModelCaps}`;
+        this.Actions.POSTING_ENTITY_OF_ITEM = `POSTING_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}`;
         dispatch({ type: this.Actions.POSTING_ENTITY_OF_ITEM, entity });
         const response = await this.requestPostEntityByItem(id, entity, data, params).catch(utils.throwError);
-        this.Actions.POST_ENTITY_OF_ITEM_SUCCESS = `POST_${_.toUpper(entity)}_OF_${this.ModelCaps}_SUCCESS`
+        this.Actions.POST_ENTITY_OF_ITEM_SUCCESS = `POST_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}_SUCCESS`
         dispatch({ type: this.Actions.POST_ENTITY_OF_ITEM_SUCCESS, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
     requestDeleteEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities]) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, method: 'DELETE' });
     deleteEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.DELETING_ENTITY_OF_ITEM = `DELETING_${_.toUpper(entity)}_OF_${this.ModelCaps}`;
+        this.Actions.DELETING_ENTITY_OF_ITEM = `DELETING_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}`;
         dispatch({ type: this.Actions.DELETING_ENTITY_OF_ITEM, entity });
         const response = await this.requestDeleteEntityByItem(id, entity).catch(utils.throwError);
-        this.Actions.ITEM_ENTITY_DELETED = `${this.ModelCaps}_${_.toUpper(entity)}_DELETED`
+        this.Actions.ITEM_ENTITY_DELETED = `${this.ModelCaps}_${(entity || '').toUpperCase()}_DELETED`
         dispatch({ type: this.Actions.ITEM_ENTITY_DELETED, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
     requestGetActivity = async (activity: TActivities[keyof TActivities], params: object = {}) => utils.request({ url: `${this.ModelName}/${activity}`, params });
     getActivity = (activity: TActivities[keyof TActivities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.FETCHING_ACTIVITY = `FETCHING_${this.ModelCaps}_${_.toUpper(activity)}`;
+        this.Actions.FETCHING_ACTIVITY = `FETCHING_${this.ModelCaps}_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.FETCHING_ACTIVITY, activity });
         const response = await this.requestGetActivity(activity, params).catch(utils.throwError);
-        this.Actions.ACTIVITY_RECEIVED = `${this.ModelCaps}_${_.toUpper(activity)}_RECEIVED`;
+        this.Actions.ACTIVITY_RECEIVED = `${this.ModelCaps}_${(activity || '').toUpperCase()}_RECEIVED`;
         dispatch({ type: this.Actions.ACTIVITY_RECEIVED, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
     requestPostActivity = async (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}) => utils.request({ url: `${this.ModelName}/${activity}`, data, method: 'POST', params });
     postActivity = (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.POSTING_ACTIVITY = `POSTING_${this.ModelCaps}_${_.toUpper(activity)}`;
+        this.Actions.POSTING_ACTIVITY = `POSTING_${this.ModelCaps}_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.POSTING_ACTIVITY, activity });
         const response = await this.requestPostActivity(activity, data, params).catch(utils.throwError);
-        this.Actions.ACTIVITY_POST_SUCCESS = `${this.ModelCaps}_${_.toUpper(activity)}_POST_SUCCESS`;
+        this.Actions.ACTIVITY_POST_SUCCESS = `${this.ModelCaps}_${(activity || '').toUpperCase()}_POST_SUCCESS`;
         dispatch({ type: this.Actions.ACTIVITY_POST_SUCCESS, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
     requestGetItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, params });
     getItemActivity = (id: string | number, activity: TActivities[keyof TActivities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.FETCHING_ITEM_ACTIVITY = `FETCHING_${this.ModelCaps}_ITEM_${_.toUpper(activity)}`;
+        this.Actions.FETCHING_ITEM_ACTIVITY = `FETCHING_${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.FETCHING_ITEM_ACTIVITY, activity });
         const response = await this.requestGetItemActivity(id, activity, params).catch(utils.throwError);
-        this.Actions.ITEM_ACTIVITY_RECEIVED = `${this.ModelCaps}_ITEM_${_.toUpper(activity)}_RECEIVED`;
+        this.Actions.ITEM_ACTIVITY_RECEIVED = `${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}_RECEIVED`;
         dispatch({ type: this.Actions.ITEM_ACTIVITY_RECEIVED, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
     requestPostItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, method: 'POST', data, params });
     postItemActivity = (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
-        this.Actions.POSTING_ITEM_ACTIVITY = `$POSTING_${this.ModelCaps}_ITEM_${_.toUpper(activity)}`;
+        this.Actions.POSTING_ITEM_ACTIVITY = `$POSTING_${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.POSTING_ITEM_ACTIVITY, activity });
         const response = await this.requestPostItemActivity(id, activity, data, params).catch(utils.throwError);
         this.Actions.ITEM_ACTIVITY_POST_SUCCESS = `${this.ModelCaps}_ITEM_ACTIVITY_POST_SUCCESS`;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,13 @@
-import { TMethod, TRequestConfig } from "../LoopFront";
+import { TRequestConfig } from "../LoopFront";
 import axios from 'axios'
 
 // Just a wrapper on axios, in case some other library is used in place of axios
 const utils = {
 
-    request: (config: TRequestConfig): ReturnType<typeof axios.request> => axios.request(config),
+    request: (config: TRequestConfig): ReturnType<typeof axios.request> => {
+        if (!axios.defaults.baseURL) { throw new Error('Error: Loopfront Base Url is not provided'); }
+        return axios.request(config)
+    },
 
     setBaseAPI_URL: (url: string) => axios.defaults.baseURL = url,
 
