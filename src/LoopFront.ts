5cc692a1374bb9b9286cf27c4d111c9718126d0f
Siraj Alam <sirajalam049@gmail.com>
2019-06-15 01:52:04 +0530

    Cancel token is supported

--- src/LoopFront.ts
+++ src/LoopFront.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from 'redux';
 import utils from './utils';
-import { Method } from 'axios';
+import { Method, CancelToken, AxiosRequestConfig } from 'axios';
 
 export type TStringObject = { [x: string]: string }
 
@@ -61,7 +61,7 @@ export const DefaultActivites = {
     REPLACE_OR_CREATE: 'replaceOrCreate',
 }
 
-export interface TRequestConfig {
+export interface TRequestConfig extends AxiosRequestConfig {
     url: string,
     params?: object,
     method?: Method,
@@ -182,125 +182,125 @@ class LoopFront<TCustomActions extends TStringObject = {}, TEntities extends TSt
 
 
     // GET All items of the model
-    requestGetItemsList = async (params: object = {}) => utils.request({ url: this.ModelName, params });
-    getItemsList = (params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestGetItemsList = async (params: object = {}, cancelToken?: CancelToken) => utils.request({ url: this.ModelName, params, cancelToken });
+    getItemsList = (params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.FETCHING_LIST });
-        const response = await this.requestGetItemsList(params).catch(utils.throwError);
+        const response = await this.requestGetItemsList(params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.LIST_RECEIVED, data: response.data, additionalDispatchData });
         return response;
     }
 
     // GET single instance of the model
-    requestGetItem = async (id: string | number, params: object = {}) => utils.request({ url: `/${this.ModelName}/${id}`, params });
-    getItem = (id: string | number, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestGetItem = async (id: string | number, params: object = {}, cancelToken?: CancelToken) => utils.request({ url: `/${this.ModelName}/${id}`, params, cancelToken });
+    getItem = (id: string | number, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.FETCHING_SINGLE_ITEM });
-        const response = await this.requestGetItem(id, params).catch(utils.throwError);
+        const response = await this.requestGetItem(id, params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.SINGLE_ITEM_RECEIVED, data: response.data, additionalDispatchData });
         return response;
     }
 
     // create a new instance
-    requestPostItem = async (data: object = {}, params: object = {}, ) => utils.request({ url: this.ModelName, method: 'POST', data, params });
-    postItem = (data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPostItem = async (data: object = {}, params: object = {}, cancelToken?: CancelToken) => utils.request({ url: this.ModelName, method: 'POST', data, params, cancelToken });
+    postItem = (data: object = {}, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.POSTING_ITEM });
-        const response = await this.requestPostItem(data, params).catch(utils.throwError);
+        const response = await this.requestPostItem(data, params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.POST_ITEM_SUCCESS, data: response.data, additionalDispatchData });
         return response;
     }
 
     // replace existing instance of the model or inserting a new one
-    requestPutItem = async (data: object = {}, params: object = {}) => utils.request({ url: this.ModelName, method: 'PUT', data, params });
-    putItem = (data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPutItem = async (data: object = {}, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: this.ModelName, method: 'PUT', data, params, cancelToken });
+    putItem = (data: object = {}, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.PUTTING_ITEM });
-        const response = await this.requestPutItem(data, params).catch(utils.throwError);
+        const response = await this.requestPutItem(data, params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.PUT_ITEM_SUCCESS, data: response.data, additionalDispatchData });
         return response;
     }
 
     // Update the existing instance of the model
-    requestPatchItem = (id: string | number, data: object, params: object = {}) => utils.request({ url: `${this.ModelName}/${id}`, method: 'PATCH', data, params });
-    patchItem = (id: string | number, data: object, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPatchItem = (id: string | number, data: object, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}`, method: 'PATCH', data, params, cancelToken });
+    patchItem = (id: string | number, data: object, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.PATCHING_ITEM });
-        const response = await this.requestPatchItem(id, data, params).catch(utils.throwError);
+        const response = await this.requestPatchItem(id, data, params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.ITEM_PATCH_SUCCESS, data: response.data, additionalDispatchData });
         return response;
     }
 
     // Delete an instance of the model
-    requestDeleteItem = async (id: string | number) => utils.request({ url: `${this.ModelName}/${id}`, method: 'DELETE' });
-    deleteItem = (id: string | number, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestDeleteItem = async (id: string | number, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}`, method: 'DELETE', params, cancelToken });
+    deleteItem = (id: string | number, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         dispatch({ type: this.Actions.DELETING_ITEM });
-        const response = await this.requestDeleteItem(id).catch(utils.throwError);
+        const response = await this.requestDeleteItem(id, params, cancelToken).catch(utils.throwError);
         dispatch({ type: this.Actions.ITEM_DELETED, data: response.data, additionalDispatchData });
         return response;
     }
 
 
-    requestGetEntityByItem = async (id: number | string, entity: TEntities[keyof TEntities], params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, params });
-    getEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestGetEntityByItem = async (id: number | string, entity: TEntities[keyof TEntities], params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, params, cancelToken });
+    getEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.FETCHING_ENTITY_OF_ITEM = `FETCHING_${(entity || '' as string).toUpperCase()}_OF_SINGLE_${this.ModelCaps}`;
         dispatch({ type: this.Actions.FETCHING_ENTITY_OF_ITEM, entity });
-        const response = await this.requestGetEntityByItem(id, entity, params).catch(utils.throwError);
+        const response = await this.requestGetEntityByItem(id, entity, params, cancelToken).catch(utils.throwError);
         this.Actions.ENTITY_OF_ITEM_RECEIVED = `${(entity || '').toUpperCase()}_OF_SINGLE_${this.ModelCaps}_RECEIVED`;
         dispatch({ type: this.Actions.ENTITY_OF_ITEM_RECEIVED, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
-    requestPostEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, ) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, data, method: 'POST', params });
-    postEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPostEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, data, method: 'POST', params, cancelToken });
+    postEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], data: object = {}, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.POSTING_ENTITY_OF_ITEM = `POSTING_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}`;
         dispatch({ type: this.Actions.POSTING_ENTITY_OF_ITEM, entity });
-        const response = await this.requestPostEntityByItem(id, entity, data, params).catch(utils.throwError);
+        const response = await this.requestPostEntityByItem(id, entity, data, params, cancelToken).catch(utils.throwError);
         this.Actions.POST_ENTITY_OF_ITEM_SUCCESS = `POST_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}_SUCCESS`
         dispatch({ type: this.Actions.POST_ENTITY_OF_ITEM_SUCCESS, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
-    requestDeleteEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities]) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, method: 'DELETE' });
-    deleteEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestDeleteEntityByItem = async (id: string | number, entity: TEntities[keyof TEntities], params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}/${entity}`, method: 'DELETE', params, cancelToken });
+    deleteEntityByItem = (id: string | number, entity: TEntities[keyof TEntities], params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.DELETING_ENTITY_OF_ITEM = `DELETING_${(entity || '').toUpperCase()}_OF_${this.ModelCaps}`;
         dispatch({ type: this.Actions.DELETING_ENTITY_OF_ITEM, entity });
-        const response = await this.requestDeleteEntityByItem(id, entity).catch(utils.throwError);
+        const response = await this.requestDeleteEntityByItem(id, entity, params, cancelToken).catch(utils.throwError);
         this.Actions.ITEM_ENTITY_DELETED = `${this.ModelCaps}_${(entity || '').toUpperCase()}_DELETED`
         dispatch({ type: this.Actions.ITEM_ENTITY_DELETED, data: response.data, entity, additionalDispatchData });
         return response;
     }
 
-    requestGetActivity = async (activity: TActivities[keyof TActivities], params: object = {}) => utils.request({ url: `${this.ModelName}/${activity}`, params });
-    getActivity = (activity: TActivities[keyof TActivities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestGetActivity = async (activity: TActivities[keyof TActivities], params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${activity}`, params, cancelToken });
+    getActivity = (activity: TActivities[keyof TActivities], params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.FETCHING_ACTIVITY = `FETCHING_${this.ModelCaps}_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.FETCHING_ACTIVITY, activity });
-        const response = await this.requestGetActivity(activity, params).catch(utils.throwError);
+        const response = await this.requestGetActivity(activity, params, cancelToken).catch(utils.throwError);
         this.Actions.ACTIVITY_RECEIVED = `${this.ModelCaps}_${(activity || '').toUpperCase()}_RECEIVED`;
         dispatch({ type: this.Actions.ACTIVITY_RECEIVED, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
-    requestPostActivity = async (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}) => utils.request({ url: `${this.ModelName}/${activity}`, data, method: 'POST', params });
-    postActivity = (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPostActivity = async (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${activity}`, data, method: 'POST', params, cancelToken });
+    postActivity = (activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.POSTING_ACTIVITY = `POSTING_${this.ModelCaps}_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.POSTING_ACTIVITY, activity });
-        const response = await this.requestPostActivity(activity, data, params).catch(utils.throwError);
+        const response = await this.requestPostActivity(activity, data, params, cancelToken).catch(utils.throwError);
         this.Actions.ACTIVITY_POST_SUCCESS = `${this.ModelCaps}_${(activity || '').toUpperCase()}_POST_SUCCESS`;
         dispatch({ type: this.Actions.ACTIVITY_POST_SUCCESS, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
-    requestGetItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, params });
-    getItemActivity = (id: string | number, activity: TActivities[keyof TActivities], params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestGetItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, params, cancelToken });
+    getItemActivity = (id: string | number, activity: TActivities[keyof TActivities], params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.FETCHING_ITEM_ACTIVITY = `FETCHING_${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.FETCHING_ITEM_ACTIVITY, activity });
-        const response = await this.requestGetItemActivity(id, activity, params).catch(utils.throwError);
+        const response = await this.requestGetItemActivity(id, activity, params, cancelToken).catch(utils.throwError);
         this.Actions.ITEM_ACTIVITY_RECEIVED = `${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}_RECEIVED`;
         dispatch({ type: this.Actions.ITEM_ACTIVITY_RECEIVED, data: response.data, activity, additionalDispatchData });
         return response;
     }
 
-    requestPostItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, method: 'POST', data, params });
-    postItemActivity = (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
+    requestPostItemActivity = async (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, cancelToken?: CancelToken, ) => utils.request({ url: `${this.ModelName}/${id}/${activity}`, method: 'POST', data, params, cancelToken });
+    postItemActivity = (id: string | number, activity: TActivities[keyof TActivities], data: object = {}, params: object = {}, cancelToken?: CancelToken, additionalDispatchData: object = {}) => async (dispatch: Dispatch<any>) => {
         this.Actions.POSTING_ITEM_ACTIVITY = `$POSTING_${this.ModelCaps}_ITEM_${(activity || '').toUpperCase()}`;
         dispatch({ type: this.Actions.POSTING_ITEM_ACTIVITY, activity });
-        const response = await this.requestPostItemActivity(id, activity, data, params).catch(utils.throwError);
+        const response = await this.requestPostItemActivity(id, activity, data, params, cancelToken).catch(utils.throwError);
         this.Actions.ITEM_ACTIVITY_POST_SUCCESS = `${this.ModelCaps}_ITEM_ACTIVITY_POST_SUCCESS`;
         dispatch({ type: this.Actions.ITEM_ACTIVITY_POST_SUCCESS, data: response.data, activity, additionalDispatchData });
         return response;
